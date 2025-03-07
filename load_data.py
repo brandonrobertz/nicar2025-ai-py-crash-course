b9ccc77c180e284e4c4181c425960ea01cc7cc95
Brandon Roberts <brandon@bxroberts.org>
2025-03-06 23:16:27 -0600

    Finish pipeline
    README still stubbed

--- load_data.py
+++ load_data.py
@@ -6,6 +6,13 @@ import json
 def load_records(filepath, encoding='utf-8'):
     if not os.path.exists(filepath):
         raise ValueError(f"Error: File '{filepath}' not found.")
+    if os.path.isdir(filepath):
+        data = []
+        for filename in os.listdir(filepath):
+            if filename.endswith(".txt"):
+                with open(os.path.join(filepath, filename), "r") as f:
+                    data.append(f.read())
+        return data
     if filepath.endswith(".csv"):
         with open(filepath, 'r', newline='', encoding=encoding) as file:
             csv_reader = csv.DictReader(file)
@@ -20,6 +27,14 @@ def load_records(filepath, encoding='utf-8'):
                 if not line or not line.strip():
                     continue
                 data.append(json.loads(line))
+        return data
+    elif filepath.endswith(".txt"):
+        data = []
+        with open(filepath, "r", encoding=encoding) as f:
+            for line in f.readlines():
+                if not line or not line.strip():
+                    continue
+                data.append(line.strip())
         return data
     else:
         raise NotImplementedError(f"Don't know how to load this file! {filepath}")
